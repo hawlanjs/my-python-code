76fb97ced264a3b7db055e4b2755098bc453ef5d
hawlan <83552463+hawlanjs@users.noreply.github.com>
2022-01-16 09:48:15 +0100

    Create if.py

---
+++ if.py
@@ -0,0 +1,12 @@
+temperature = 15 #30 or 25 
+
+if temperature > 30:
+    print("it's a hot day")
+    print("drink plent of water")
+elif temperature > 20:
+    print("it's a nice day bro")
+elif temperature > 10:
+    print("it's a bit coll")
+elif temperature > 5:
+    print("it's coll")
+print("done")
